a5ee08200f440dd1653a1dffe613d521b70d7bcb
Mohsen Abedi <mohsenabedi68@gmail.com>
2025-01-11 13:33:45 +0100

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -2,7 +2,26 @@
 
 cd /home/ec2-user/helloworld
 
-pkill -f "python3 /home/ec2-user/helloworld/helloworld.py"
+PID=$(sudo lsof -t -i :80)
+
+if [ -z "$PID" ]; then
+  echo "No application is using port 80."
+  exit 0
+else
+  echo "Application using port 80 found with PID: $PID"
+  
+  # Kill the process
+  echo "Killing process with PID: $PID"
+  sudo kill -9 $PID
+  
+  # Verify if the process was successfully killed
+  if [ $? -eq 0 ]; then
+    echo "Process on port 80 has been killed successfully."
+  else
+    echo "Failed to kill the process on port 80."
+    exit 1
+  fi
+fi
 
 echo "Flask application killed."
 
