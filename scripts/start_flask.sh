c700c70959cf1430f5b1e5f181c53bf94add8f25
Mohsen Abedi <mohsenabedi68@gmail.com>
2025-01-11 13:18:58 +0100

    Update start_flask.sh

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -6,6 +6,6 @@ pkill -f "python3 /home/ec2-user/helloworld/helloworld.py"
 
 echo "Flask application killed."
 
-python3 helloworld.py 
+sudo python3 helloworld.py 
 
 echo "Flask application started."
