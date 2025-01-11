36b3cf4599c452fcdbc3c7fac1a87742e0735fb0
Mohsen Abedi <mohsenabedi68@gmail.com>
2025-01-11 13:54:28 +0100

    adding new validate service

---
+++ scripts/validate_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Wait until the Flask app is available
+for i in {1..10}; do
+  curl -f http://localhost:80 && exit 0
+  echo "Waiting for Flask app..."
+  sleep 5
+done
+
+echo "Flask app failed to start."
+exit 1
