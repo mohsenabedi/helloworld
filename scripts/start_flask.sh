c740997305aa04567db86c85033fe42a897d1215
Mohsen Abedi <mohsenabedi68@gmail.com>
2025-01-11 14:04:04 +0100

    testing deployment via root

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -3,7 +3,6 @@
 # Navigate to the application directory
 cd /home/ec2-user/helloworld || { echo "Failed to change directory"; exit 1; }
 
-# Check if a process is using port 80
 PID=$(sudo lsof -t -i :80)
 
 if [ -z "$PID" ]; then
@@ -11,11 +10,9 @@ if [ -z "$PID" ]; then
 else
   echo "Application using port 80 found with PID: $PID"
   
-  # Kill the process
   echo "Killing process with PID: $PID"
   sudo kill -9 "$PID"
   
-  # Verify if the process was successfully killed
   if [ $? -eq 0 ]; then
     echo "Process on port 80 has been killed successfully."
   else
@@ -25,8 +22,8 @@ fi
 
 echo "Flask application killed."
 
-# Start the Flask application in the background
 echo "Starting Flask application..."
+
 sudo nohup python3 helloworld.py > flask.log 2>&1 &
 
 # Verify if the Flask app started
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -3,7 +3,7 @@
 for i in {1..10}; do
   curl -f http://localhost:80 && exit 0
   echo "Waiting for Flask app..."
-  sleep 1
+  sleep 5
 done
 
 echo "Flask app failed to start."
